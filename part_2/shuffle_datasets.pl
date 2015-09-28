01f21fb7eaadcc097a102b9ab76b70ffe531b7bf
lonelyjoeparker <joe@lonelyjoeparker.com>
2015-09-28 12:46:38 +0100

    Adding specific student input files as well as script to shuffle tips for the students' alignments.

---
+++ part_2/shuffle_datasets.pl
@@ -0,0 +1,6 @@
+#! /usr/bin/perl
+# simple script to shuffle tips in an alignment a random number of times in (2...5)
+
+$replicate_datasets = 9;	# number of students to generate
+
+open(IN,$ARGV[0]);
