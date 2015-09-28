de74c7017b9513f8023ba2f6da043ef41a609c99
lonelyjoeparker <joe@lonelyjoeparker.com>
2015-09-28 16:57:55 +0100

    Final files for the practical

--- commands.sh
+++ commands.sh
@@ -13,16 +13,19 @@
 #									#
 #####################################
 
+## The files needed for this practical are also available at https://github.com/lonelyjoeparker/MSc-phylogenetics-lab
+
 
 ## Part 1 ##
 # You'll need to be in the directory '~/Desktop/phylogenetics/part_1'
 
-# building a tree - first go
-muscle -in orchids.short.initial.fa -out orchids.short.out -maxiters 2
 # (view output in CONTEXT graphical user interface (GUI)
 # (either close it once you're done and reopen when you need to)
 # (or leave it open if the Pi isn't running _too_ slowly)
 java -jar ../software/CONTEXT-PhylogenomicDatasetBrowser-v0.8.1.jar
+# building a tree - first go
+muscle -in orchids.short.initial.fa -out orchids.short.out -maxiters 2
+# (view output in CONTEXT graphical user interface (GUI))
 # convert muscle output to phylip format for RAxML
 java -jar ../software/PrepareFilesForPaml.jar orchids.short.out
 # build a phylogeny in RAxML
@@ -46,20 +49,30 @@ java -jar ../software/PrepareFilesForPaml.jar orchids.short.filtered.out
 # You'll need to be in the directory '~/Desktop/phylogenetics/part_2'
 
 # building a tree, genus A
-muscle
-java -jar PrepareFilesForPaml.jar
-raxmlHPC
+muscle -in genus_A_master_ungapped.fa -out genus_A_aligned.out  -maxiters 2
+java -jar PrepareFilesForPaml.jar genus_A_aligned.out
+../software/raxmlHPC -m GTRCAT -n genus_A -p 222  -s genus_A_aligned.out.stops.removed.phy 
 # (view output in FigTree GUI)
 
 # building a tree, genus B
-muscle
+muscle -in genus_B_master_ungapped.fa -out genus_B_aligned.out  -maxiters 2
 java -jar PrepareFilesForPaml.jar
-raxmlHPC
+../software/raxmlHPC -m GTRCAT -n genus_B -p 2222  -s genus_B_aligned.out.stops.removed.phy 
 # (view output in FigTree GUI)
 
 # (running the BEAST MCMC)
 #java -jar beast.jar mc.paml_MCMC_slow.xml
 
+# (look at output in Tracer GUI)
+#java -jar tracer.jar
+
+# (look at output dated tree in FigTree GUI)
+
 
 ## Part 3 ##
 # You'll need to be in the directory '~/Desktop/phylogenetics/part_3'
+# building a tree, genus A
+muscle -in Trich_ITS.NXS.edit.fa -out Trich_ITS.aligned.fa  -maxiters 2
+java -jar PrepareFilesForPaml.jar Trich_ITS.aligned.fa 
+../software/raxmlHPC -m GTRCAT -n Trich_ITS -p 333  -s Trich_ITS.aligned.fa.stops.removed.phy 
+# (view output in FigTree GUI)
